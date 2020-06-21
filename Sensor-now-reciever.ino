196070bd2752aac6d95fffcc8507c3f64af2316d
binoys <binoy.sundaran@gmail.com>
2020-06-21 20:09:59 +0530

    added oled display for reciever

--- Sensor-now-reciever.ino
+++ Sensor-now-reciever.ino
@@ -1,52 +1,72 @@
 #include <ESP8266WiFi.h>
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
 #include <espnow.h>
+
+const int MAX_DISTANCE = 62; //Sump tank
+const int MIN_LEVEL = 2;
+
+const int MIN_DISTANCE = 10; //Overhead tank
+
+// Initialize the OLED display using Wire library
+SSD1306Wire  display(0x3c, D2, D1);  //D2=SDK  D1=SCK  As per labeling on NodeMCU
+const int buzzPin = D5;
 
 // Structure example to receive data
 // Must match the sender structure
 typedef struct struct_message {
-    int id;
-    float distance;
+  int id;
+  float distance;
 } struct_message;
 
 // Create a struct_message called myData
 struct_message myData;
 
-// Create a structure to hold the readings from each board
-struct_message board1;
-struct_message board2;
+void setup() {
 
-// Create an array with all the structures
-struct_message boardsStruct[2] = {board1, board2};
+  pinMode(buzzPin, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("");
 
-// Callback function that will be executed when data is received
-void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) {
+  delay(1000);
+  Serial.println("Initializing OLED Display");
+  display.init();
 
-  char macStr[18];
-  Serial.print("Packet received from: ");
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-    mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.println(macStr);
-  
-  
-  memcpy(&myData, incomingData, sizeof(myData));
-  
-  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);;
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_24);
 
-  boardsStruct[myData.id - 1].distance = myData.distance;
-  
-  Serial.print("distance: ");
-  Serial.println(myData.distance);
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
 
-  if (myData.distance < 200) {
-    start_buzz();  
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
   }
-  
-  check_and_quiet();
-  
 
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
-const int buzzPin = D2;
+// Callback function that will be executed when data is received
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+
+  memcpy(&myData, incomingData, sizeof(myData));
+  //Serial.print("Bytes received: ");
+  //Serial.println(len);
+
+  if ( millis() % 1000 < 10) { //write once in a while
+    Serial.print(myData.id);
+    Serial.print("]: distance = ");
+    Serial.println(myData.distance);
+  }
+}
+
+
+
 uint16_t time_elapsed = 0;
 uint16_t buzz_start = 0;
 boolean in_buzzing_state = false;
@@ -60,51 +80,114 @@ void start_buzz() {
     Serial.println("buzing ....");
     digitalWrite(buzzPin, HIGH);
   }
-  
+
 }
 
 void check_and_quiet() {
 
+
   time_elapsed = millis() - buzz_start;
-  Serial.print("quiet ....");
-  Serial.println(time_elapsed);
+  //Serial.print("quiet ....");
+  //Serial.println(time_elapsed);
+
   if (time_elapsed > 500) {
     digitalWrite(buzzPin, LOW);
   }
 
+  //leave a delay for buzing to resume
   if (time_elapsed > 1000) {
-    digitalWrite(buzzPin, HIGH);
-  }
-
-  if (time_elapsed > 1500) {
-    digitalWrite(buzzPin, LOW);
     in_buzzing_state = false;
   }
-  
+
 }
 
- 
-void setup() {
-  // Initialize Serial Monitor
-  Serial.begin(115200);
-  pinMode(buzzPin, OUTPUT);
-  
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
+boolean start_up = true;
+int distance = 0;
+int level = 0;
 
-  // Init ESP-NOW
-  if (esp_now_init() != 0) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
-  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
-  esp_now_register_recv_cb(OnDataRecv);
-}
+int distance_to_empty = 0; // sump tank
+int distance_to_fill = 0; // overhead tank
+
+int prev_distance_to_empty = 0;
+int prev_distance_to_fill = 0;
 
 void loop() {
-  
+
+  if (start_up) {
+
+    displayWrite("Hello");
+    delay(1000);
+    start_up = false;
+
+  }else {
+
+    if (myData.id == 1) {
+
+        Serial.print("inside borad 1 reading");
+
+        distance_to_fill = ceil(myData.distance);
+        if (distance_to_fill < MIN_DISTANCE) {
+          start_buzz();
+        }
+
+        Serial.print(", distance_to_fill = " + String(distance_to_fill));
+        Serial.print(", prev_distance_to_fill = " + String(prev_distance_to_fill));
+
+        if ( distance_to_fill != prev_distance_to_fill) {
+          updateDisplay();
+        }
+        prev_distance_to_fill = distance_to_fill;
+        Serial.println("..done");
+
+    }else if (myData.id == 2) {
+
+      Serial.print("inside borad 2 reading");
+
+      distance_to_empty = MAX_DISTANCE - floor(myData.distance);
+      if (distance_to_empty < 0) distance_to_empty = 0;
+
+      if (distance_to_empty < MIN_LEVEL && distance_to_empty != 0) {
+        start_buzz();
+      }
+
+      Serial.print(", distance_to_empty = " + String(distance_to_empty));
+      Serial.print(", prev_distance_to_empty = " + String(prev_distance_to_empty));
+
+      if ( distance_to_empty != prev_distance_to_empty) {
+        updateDisplay();
+      }
+      prev_distance_to_empty = distance_to_empty;
+
+    }
+
+    check_and_quiet();
+    delay(500);
+  }
+
+}
+
+void displayWrite(String str) {
+
+    display.clear();
+    display.drawString(0, 26, str);
+    display.display();
+}
+
+void updateDisplay() {
+
+    display.clear();
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 0, "[" +String(myData.id) + "] " + String(myData.distance) + "cm");
+
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 20, "to fill:" );
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(80, 15, String(distance_to_fill));
+
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 45, "to empty:" );
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(80, 40, String(distance_to_empty));
+
+    display.display();
 }
